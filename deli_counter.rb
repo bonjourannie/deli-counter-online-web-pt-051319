20a3d8d501c9e460e0d5a1ede62b8dd422362726
Annie Truex <artruex@gmail.com>
2019-05-17 02:09:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,8 @@ def line(line)
 end
 
 def take_a_number(katz_deli, name)
+  if line == 0 
+    puts "The line is "
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
